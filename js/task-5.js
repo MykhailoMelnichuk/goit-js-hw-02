0c218f065f70ad1610f3e165cd3bfac9d2c74735
MykhailoMelnichuk <mmdcomp@ukr.net>
2019-10-19 00:17:26 +0300

    Module 2 hw 06 tasks1-6

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,10 @@
-console.log('Задание 5')
+console.log('Задание 5');
+const checkForSpam = function checkForSpam(str) {
+    str = str.toLowerCase();
+    const isStrForbiddenWord = str.includes('spam') || str.includes('sale');
+    return isStrForbiddenWord;
+  };
+  console.log(checkForSpam('Latest technology news'));
+  console.log(checkForSpam('JavaScript weekly newsletter'));
+  console.log(checkForSpam('Get best sale offers now!'));
+  console.log(checkForSpam('[SPAM] How to earn fast money?'));
--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,25 @@
-console.log('Задание 6')
+console.log('Задание 6');
+let input;
+const numbers = [];
+let total = 0;
+let inputNamber;
+while (input !== null) {
+    input = prompt('Введите число:');
+    inputNamber = Number(input);
+    if (Number.isNaN(inputNamber)) {
+        alert('Было введено не число, попробуйте еще раз.');
+    } else if (inputNamber !== 0) {
+        numbers.push(inputNamber);
+    }
+}
+
+if (numbers.length === 0) {
+    alert('Вы не ввели ни одного числа.');
+} else {
+    for (let i = 0; i < numbers.length; i += i) {
+        total = total + numbers[i];
+    }
+    console.log(`Общая сумма чисел равна ${total}.`);
+};
+
+console.log(`Общая сумма чисел равна ${total}.`);
